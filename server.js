fa1eef3ccb3bb2ccdb7581d7247cf38defadd278
andrey.egorov <andrey.egorov@taskdata.com>
2019-05-01 11:44:41 +0300

    Added bot header's analys

--- server.js
+++ server.js
@@ -7,8 +7,42 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/gi-front'));
 
+function detectBot(userAgent){
+    const bots = [
+      'googlebot',
+      'bingbot',
+      'yandexbot',
+      'duckduckbot',
+      'slurp',
+      'twitterbot',
+      'pinterest',
+      'vkshare', 
+      'yandexaccessibilitybot',   
+      'yandexmetrika',   
+      'yandexscreenshotbot',   
+      'yandexblogs',   
+      'yandeximages'   
+    ];
+  
+    const agent = userAgent.toLowerCase()
+  
+    for(const bot of bots){
+      if(agent.indexOf(bot) > -1){
+        console.log('bot detected',bot,agent)
+        return true
+      }
+    }
+  
+    console.log('not bots found')
+    return false
+  }
+
 app.get('/*', function(req,res) {
-    
+
+const isBot = detectBot(req.headers['user-agent']);
+
+if(isBot) res.send('This is bot')
+
 res.sendFile(path.join(__dirname, '/dist/gi-front/index.html'));
 });
 
