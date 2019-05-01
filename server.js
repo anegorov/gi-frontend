329e1c4f1357bf281164e65ef760537aca8c31e5
andrey.egorov <andrey.egorov@taskdata.com>
2019-05-01 12:43:58 +0300

    bots

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const fetch = require('node-fetch');
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -40,11 +40,16 @@ function detectBot(userAgent){
 app.get('/*', function(req,res) {
 
 const isBot = detectBot(req.headers['user-agent']);
-
-if(isBot) res.send('This is bot')
-
-res.sendFile(path.join(__dirname, '/dist/gi-front/index.html'));
+    if(isBot) {
+        // res.send('This is bot');
+    fetch(`https://guidein.herokuapp.com/render/domik-i-polka-svoimi-rukami.html`)
+      .then(res => res.text())
+      .then(body => {
+      res.send(body.toString());
+      });
+    }else{
+        res.sendFile(path.join(__dirname, '/dist/gi-front/index.html'));
+    }
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
